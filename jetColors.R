ca84366c667cdf2d0fe360e127b4364864b4e589
David Buckley <dnbuckle@usc.edu>
2019-09-05 16:37:55 -0700

    asdf

---
+++ jetColors.R
@@ -0,0 +1,8 @@
+library(circlize)
+
+jetColors <- function(){
+  jet <- colorRamp2(seq(0, 1, 0.125),
+                    c("#00007F", "blue", "#007FFF", "cyan",
+                      "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
+  return(jet)
+}
