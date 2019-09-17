6b2331709c6778b770a3de8ae6357b780621d9ec
Salhia Lab <sal_lab@Salhias-iMac.local>
2019-09-17 16:13:04 -0700

    asdf

--- getRestrictionLocs.R
+++ getRestrictionLocs.R
@@ -4,27 +4,31 @@ library(BSgenome.Hsapiens.UCSC.hg19)
 # get restriction locations for specified rescriction sequence
 # default MSP1
 
-.combineInRange <- function(gr, range){
-  end(gr) <- end(gr) + range
-  gr <- reduce(gr)
-  end(gr) <- end(gr) - range
-  return(gr)
+.segmentCHR <- function(chr){
+  starts <- start(chr)
+  ends <- end(chr)
+  x <- GRanges(cbind.data.frame(chr = unique(seqnames(chr)),
+                                 start = ends[1:(length(ends) - 1)],
+                                 end = starts[2:length(starts)]))
+  return(x)
 }
 
-getRestrictionLocs <- function(sequence = "CCGG", genome = "hg19", cores = 4, proximity = 1000){
+getRestrictionLocs <- function(sequence = "CCGG", genome = "hg19", cores = 4, minWidth = 20, maxWidth = 200){
   if (genome == "hg19") {
     Hsapiens <- BSgenome.Hsapiens.UCSC.hg19
   } else if (genome == "hg38") {
     Hsapiens <- BSgenome.Hsapiens.UCSC.hg38
   } else message("invalid genome selection")
   chrs <- names(Hsapiens)[1:24]
-  locs <- mclapply(chrs, function(x) start(matchPattern("CCGG", 
+  locs <- mclapply(chrs, function(x) start(matchPattern(sequence, 
                                               Hsapiens[[x]])), mc.cores = cores)
-  gr <- do.call(c, lapply(1:24, function(x) GRanges(names(Hsapiens)[x], 
-                                            IRanges(locs[[x]], width = length(sequence)))))
+  gr <- lapply(1:24, function(x) GRanges(names(Hsapiens)[x], 
+                                IRanges(locs[[x]], width = nchar(sequence))))
+  gr <- do.call(c, lapply(gr, .segmentCHR))
   seqinfo(gr) <- seqinfo(Hsapiens)[seqlevels(gr)]
-  gr <- .combineInRange(gr, range = proximity)
-  gr <- gr[width(gr) > length(sequence)]
+  gr <- gr[width(gr) >= minWidth]
+  gr <- gr[width(gr) <= maxWidth]
+  export.bed(gr, "test.bed")
   return(gr)
 }
 
