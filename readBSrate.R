3a1c145bdf93850c1337a2cb24e5b8cdfb2b05a6
David Buckley <dbuckley@Davids-MBP.attlocal.net>
2020-03-24 12:13:14 -0700

    update

--- readBSrate.R
+++ readBSrate.R
@@ -7,11 +7,11 @@ readBSrate <- function(file, summary = F) {
     s <- strsplit(s$V1, " = ")
     pos <- unlist(strsplit(s[[2]][2], "\t"))
     neg <- unlist(strsplit(s[[3]][2], "\t"))
-    df <- data.frame(ovarall.conv.rate = s[[1]][2],
-                     pos.conv.rate = pos[1],
-                     neg.conv.rate = neg[1],
-                     pos.conv.count = pos[2],
-                     neg.conv.count = neg[2],
+    df <- data.frame(ovarall.conv.rate = as.numeric(s[[1]][2]),
+                     pos.conv.rate = as.numeric(pos[1]),
+                     neg.conv.rate = as.numeric(neg[1]),
+                     pos.conv.count = as.numeric(pos[2]),
+                     neg.conv.count = as.numeric(neg[2]),
                      stringsAsFactors = F)
   } else {
     df <- read.table(file, skip = 3, header = T)
